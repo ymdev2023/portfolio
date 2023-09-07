0c1b03fa15686fcb6715bc565a98027fce33e439
ym <ymdev2023@gmail.com>
2023-09-07 09:01:25 +0900

    update

--- 5_Flutter/toonflix/lib/models/webtoon_model.dart
+++
@@ -1,8 +0,0 @@
-class WebtoonModel {
-  late final String title, thumb, id;
-
-  WebtoonModel.fromJson(Map<String, dynamic> json)
-      : title = json['title'],
-        thumb = json['thumb'],
-        id = json['id'];
-}
